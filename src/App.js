2bed1c06a236cec810c99f9d70087a5cebbefefa
Adriano Biolchi <adrianobiolchi@outlook.com>
2019-07-17 14:16:21 -0300

    finalizando desafio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header/Header';
+import PostList from './components/PostList/PostList';
+
 function App() {
-  return <></>;
+  return (
+    <>
+     <Header /> 
+     <PostList /> 
+    </>
+  )
 }
 
 export default App;
